f62cdbfa3bdcf1f28ebec6931733e349b655e3f2
joohnyoung <823838890@qq.com>
2023-05-30 16:23:46 +0800

    基本框架搭建完成

--- app/src/main/java/com/example/anonymous_community_app/MainActivity.kt
+++ app/src/main/java/com/example/anonymous_community_app/MainActivity.kt
@@ -1,36 +1,28 @@
 package com.example.anonymous_community_app
 
 import android.os.Bundle
-import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.anonymous_community_app.databinding.ActivityMainBinding
+import com.example.anonymous_community_app.ui.me.MeViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_post, R.id.navigation_courese, R.id.navigation_message, R.id.navigation_me
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        // 登陆情况判断
+        viewModel.login = false
     }
 }
